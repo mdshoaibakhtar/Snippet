95e4d280a50c20e14318b4ace9d1742bb437dfc7
Md Shoaib <mdshoaibakhtar966@gmail.com>
2025-09-20 20:13:34 +0530

    Update secrets

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://tlwzxlhlbzcdtuvbmgul.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRsd3p4bGhsYnpjZHR1dmJtZ3VsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgzNzA1NzYsImV4cCI6MjA3Mzk0NjU3Nn0.s1BzRyGKrcoqYzOcjKyivSaoPyO8kS4C_uxMiaakVSw"
-
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
 if (!supabaseUrl || !supabaseAnonKey){
     throw new Error("Missing Supabase environment variables")
 }
